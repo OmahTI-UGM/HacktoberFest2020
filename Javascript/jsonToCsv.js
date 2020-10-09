09b06e7c935a2ca0bf6df5c93274bf56bc13ec38
DelphicGen <gennardokidam@gmail.com>
2020-10-09 17:32:23 +0700

    Convert json to csv

---
+++ Javascript/jsonToCsv.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const promise = async () => await fs.promises.readFile('./data.json');
+
+(
+    async () => {
+        const json = JSON.parse(await promise());
+        const data = json.data;
+        const fields = Object.keys(data[0]);
+        
+        const replacer = (key, value) => value === null ? '' : value
+
+        let csv = data.map(row => fields.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','));
+
+        csv.unshift(fields);
+        csv = csv.join('\r\n');
+
+        console.log(csv);
+    }
+)()
